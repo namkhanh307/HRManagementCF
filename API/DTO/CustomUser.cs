28a81935621fd23e46822875214b6b6a4572f363
namkhanh307 <nguyenvietnamkhanhbl@gmail.com>
2024-05-15 12:49:46 +0700

    update

---
+++ API/DTO/CustomUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace API.DTO
+{
+    public class CustomUser : IdentityUser
+    {
+        public string FirstName { get; set; } = null!;
+
+        public string LastName { get; set; } = null!;
+    }
+}
---
+++ API/Models/HRMDbContext.cs
@@ -0,0 +1,17 @@
+﻿using API.DTO;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Models
+{
+    public class HRMDbContext : IdentityDbContext<CustomUser>
+    {
+        public HRMDbContext()
+        {
+        }
+        public HRMDbContext(DbContextOptions<HRMDbContext> options)
+       : base(options)
+        {
+        }
+    }
+}
